564a84583b2cb673018045969f222f37362b5cb7
Loli Admin <Loli Admin@DESKTOP-H4SR9KC>
2024-11-29 13:57:31 +0300

    Добавьте файлы проекта.

---
+++ tusck/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace tusck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("задайте размер массива");
+            int n = int.Parse( Console.ReadLine());
+            int[] a = new int [n];
+            Console.WriteLine("Массив");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Random rand = new Random();
+                
+                
+                
+
+                a[i] = rand.Next(-127, 128);
+                Console.Write(a[i] + " ");
+            }
+            
+            
+            
+        }
+    }
+}
